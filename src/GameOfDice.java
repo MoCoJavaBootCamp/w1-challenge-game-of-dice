6ce1753e94e47d5fa697dea2dbf787a107e7e461
azdoolittle <drewdoolittle@gmail.com>
2020-04-23 14:23:18 -0400

    if conditions correctly sort when roll == 1

--- src/GameOfDice.java
+++ src/GameOfDice.java
@@ -3,24 +3,21 @@ Pseudocode
 import Scanner
 import Random
 declare roll dice method
-declare vars for scanner, random, sum, roll 1, roll 2
+declare vars for scanner, random, score, roll 1, roll 2, roll again
 greet user
 prompt user to roll
 define while loop
-    check for a single 1
-        stop roll
     check for a double 1
-        add 25 to sum
-        add sum to score
+        add 25 to score
+    check for a single 1
+        0 to score
      else
-        add r1, r2
-        add sum to score
-    initialize roll 1 and roll 2
-        initialize sum
+        add r1, r2 to score
     print rolls
     print score
-    prompt to roll again
-        no: thank you for playing
+    prompt to roll again.
+  if no or over 100:
+    thank you for playing
 
  */
 import java.util.Scanner;
@@ -31,35 +28,35 @@ public class GameOfDice {
         Random random = new Random();
         Scanner input = new Scanner(System.in);
         int score = 0;
-        int roll1 = 0;
-        int roll2 =0;
+        int roll1, roll2;
         String rollAgain = "yes";
         String greeting = "Welcome! Let's Play Roll Dice!";
         String rollPrompt = "Roll again?";
 
         System.out.printf("%s", greeting);
-        while (score < 100) {
-            roll1 = random.nextInt(7) + 1;
-            roll2 = random.nextInt(7) + 1;
+        while (rollAgain.equals("yes")) {
+                roll1 = random.nextInt(6) + 1;
+                roll2 = random.nextInt(6) + 1;
 
-            if (roll1 == 1 && roll2 != 1) {
-                rollAgain = "no";
-            } else if (roll1 != 1 && roll2 == 1) {
-                rollAgain = "no";
-            } else if (roll1 == 1 && roll2 == 1) {
-                score += 25;
-                rollAgain = "yes";
-            } else {
-                score += roll1 + roll2;
+                if (roll1 == 1 && roll2 == 1) {
+                    score += 25;
+                } else if (roll1 == 1 || roll2 == 1) {
+                    score += 0;
+                } else {
+                    score += roll1 + roll2;
+                }
+
+                System.out.printf("\nYour rolls %d & %d", roll1, roll2);
+                System.out.printf("\nScore: %d", score);
+                System.out.printf("\n%s ", rollPrompt);
+                rollAgain = input.nextLine();
+
+            if (score > 100) {
+                System.out.println("\nYou have scored over 100");
+                break;
             }
-
-            System.out.printf("\nYour rolls %d & %d", roll1, roll2);
-            System.out.printf("\nScore: %d", score);
-            System.out.printf("\n%s %s", rollPrompt, rollAgain);
-            rollAgain = "yes";
         }
 
-        System.out.println("\nYou have scored over 100\n" +
-                "Thank you for playing!");
+        System.out.println("Thank you for playing!");
     }
 }
